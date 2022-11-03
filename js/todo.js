d7015acbc9dfc010e4522d95b4af54204760526b
bboguma <okiyooj@naver.com>
2022-11-03 23:50:43 +0900

    Update todo.js
    ~7.1

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input"); // === document.querySelector("#todo-form input")
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newToDo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault()
     const newToDo = toDoInput.value;
-    console.log(toDoInput.value);
     toDoInput.value = "";
-    console.log(newToDo, toDoInput.value);
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
