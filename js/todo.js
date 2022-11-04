f5235a4e78562ab39c05a19164e209144f8fb685
bboguma <okiyooj@naver.com>
2022-11-04 15:06:03 +0900

    Update todo.js
    7.5

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,7 @@ const toDoList = document.querySelector("#todo-list");
 
 const TODOS_KEY = "toDos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -38,16 +38,11 @@ function handleToDoSubmit(event) {
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-// function sayHello(item){
-//     console.log("Hello!", item);
-// }
-
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
-    console.log(parsedToDos);
-    // parsedToDos.forEach(sayHello);
-    parsedToDos.forEach((item) => console.log("Hello!", item));
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
 }
 
