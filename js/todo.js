a0ed857b5f77bb7155c864a99d2985cb17693ae7
bboguma <okiyooj@naver.com>
2022-11-04 10:42:52 +0900

    Update todo.js
    ~7.3

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,26 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input"); // === document.querySelector("#todo-form input")
 const toDoList = document.querySelector("#todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("toDos", toDos);
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newToDo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -14,7 +29,10 @@ function handleToDoSubmit(event) {
     event.preventDefault()
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
